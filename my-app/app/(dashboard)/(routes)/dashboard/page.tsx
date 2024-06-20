cd4cfbba743bc2b01f8364d5c0f3623aeb983687
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-06-20 12:41:57 +0600

    sidebarworking

--- my-app/app/(dashboard)/(routes)/dashboard/page.tsx
+++ my-app/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,7 +5,7 @@ const DashboardPage=()=> {
   return (
     <div>
         <p>Dashboard Page(Protected)</p>
-        <UserButton afterSignOutUrl="/"/>
+        
     </div>
     );
 }
---
+++ my-app/app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+import Navbar from "@/components/navbar";
+import Sidebar  from "@/components/sidebar";
+
+const DashboardLayout = async ({ children }: PropsWithChildren) => {
+    return (
+        <div className="h-full relative">
+          <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
+            <Sidebar/>
+          </div>
+    
+          <main className="md:md:pl-72">
+            <Navbar />
+            {children}
+          </main>
+        </div>
+      );
+};
+
+export default DashboardLayout;
