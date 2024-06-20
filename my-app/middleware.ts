ab530ac2743ce80fecac0153762ffde55ef0cdb3
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-06-20 10:23:51 +0600

    updated middleware

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+import {
+    clerkMiddleware,
+    createRouteMatcher
+  } from '@clerk/nextjs/server';
+  
+  const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+  ]);
+  
+  export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+  
+  export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
